bce75401b23d2efd0bf51c4478d62a210e20c89a
Shubham Das <skshubhamdas@gmail.com>
2024-08-13 11:11:13 +0530

    Clean up

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -13,6 +13,19 @@ export const getTrendingMovies = async (req, res) => {
 	}
 };
 
+export const getTrendingSeries = async (req, res) => {
+	try {
+		// Fetch all movies of type `series` from database
+		const movies = await Movie.find({ type: 'series' })
+			.sort({ num_mflix_comments: -1 })
+			.limit(10)
+			.exec();
+		res.status(200).json(movies);
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
+};
+
 export const getMovieById = async (req, res) => {
 	try {
 		const movie = await Movie.findById(req.params.id).select('title'); // Fetch movies by ID
@@ -26,17 +39,17 @@ export const getMovieById = async (req, res) => {
 };
 
 export const createMovie = async (req, res) => {
-	try {
-		data = req.body;
-		const name = data.name;
-		const quantity = data.quantity;
-		const price = data.price;
+	// try {
+	// 	data = req.body;
+	// 	const name = data.name;
+	// 	const quantity = data.quantity;
+	// 	const price = data.price;
 
-		// const newMovie = new Movie({ name, quantity, price });
-		// await newMovie.save();
+	// 	// const newMovie = new Movie({ name, quantity, price });
+	// 	// await newMovie.save();
 
-		res.status(201).json({ message: 'Product created!' });
-	} catch (error) {
-		res.status(400).json({ message: error.message });
-	}
+	// 	res.status(201).json({ message: 'Product created!' });
+	// } catch (error) {
+	// 	res.status(400).json({ message: error.message });
+	// }
 };
--- server/routes/movieRoute.js
+++ server/routes/movieRoute.js
@@ -1,10 +1,18 @@
 import express from 'express';
-import { getTrendingMovies, getMovieById, createMovie } from '../controllers/movieController.js';
+import {
+	getTrendingMovies,
+	getTrendingSeries,
+	getMovieById,
+	createMovie,
+} from '../controllers/movieController.js';
 
 const router = express.Router();
 
 // Retrieve trending movies
 router.get('/trending', getTrendingMovies);
+
+// Retrieve trending series
+router.get('/series/trending', getTrendingSeries);
 
 // GET /api/movies/:id - Retrieve a movie by ID
 router.get('/:id', getMovieById);
